6e7bcb27687a72f1af3e787a8fabd750eaa34065
mityu <mityu.mail@gmail.com>
2022-08-19 20:10:29 +0900

    Fix build fails with small or tiny feature

--- src/drawscreen.c
+++ src/drawscreen.c
@@ -143,7 +143,12 @@ update_screen(int type_arg)
 
     // Postpone the redrawing when it's not needed and when being called
     // recursively.
-    if (!(redrawing() || getting_user_input) || updating_screen)
+    if (!(redrawing()
+#ifdef FEAT_EVAL
+		|| getting_user_input
+#endif
+		)
+	    || updating_screen)
     {
 	redraw_later(type);		// remember type for next time
 	must_redraw = type;
